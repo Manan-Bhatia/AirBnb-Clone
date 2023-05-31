9f3c5546598c7a90e9ca55bc1d57209918c2086a
Manan Bhatia <mananbhatia02@gmail.com>
2023-05-31 18:20:17 +0530

    Nav and Hero Section Updated

---
+++ src/Hero.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import heroImg from "./assets/photo-grid.png";
+
+export default function Hero() {
+    return (
+        <div className="Hero">
+            <img src={heroImg} alt="Hero Image" className= "Hero-Image"/>
+            <h1>Online Experiences</h1>
+            <p>
+                Join unique interactive activities led by one-of-a-kind
+                hosts—all without leaving home.
+            </p>
+        </div>
+    );
+}
---
+++ src/Nav.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import airBnbLogo from './assets/airbnb-logo.png'
+
+export default function Nav()
+{
+    return(
+        <header>
+            <img src={airBnbLogo} alt="Logo" className = "Header-Image"/>
+        </header>
+    )
+}
